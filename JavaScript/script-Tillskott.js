808d7823cae889a61724801d376f64f546f8c30e
VerGui <veronica.guizzardi@gmail.com>
2021-09-16 15:39:53 +0200

    lagt in flera skor

--- JavaScript/script-Tillskott.js
+++ JavaScript/script-Tillskott.js
@@ -8,30 +8,23 @@ function Product(brand, name, price, img, bye, info) {
 
 }
 
-<<<<<<< HEAD
+
 let product1 = new Product('Goodlife', '15 x Goodlife, 50 gr', '159 :-', 'images/tillskott/goodlife.jpg', 'Lägg till', 'info');
 let product2 = new Product('NOCCO - No Carbs Company', '´24 x NOCCO BCAA, 330 ml', '429 :-', 'images/tillskott/nocco.jpg', 'Lägg till', 'info');
 let product3 = new Product('Star Nutrition', 'Peanut Butter, 1 kg', '89 :-', 'images/tillskott/peanutButter.jpg', 'Lägg till', 'info');
 let product4 = new Product('Pro Brands', 'Rehydrate, 20 tabs, Raspberry', '22 :-', 'images/tillskott/rehydrate.jpg', 'Lägg till', 'info');
 let product5 = new Product('Star Nutrition', 'Beta-alanine, 320 g', '149 :-', 'images/tillskott/betaAlanine.jpg', 'Lägg till', 'info');
+let product6 = new Product('', 'Omega3', '49kr', 'images/tillskott/coffein.jpg', 'Lägg till', 'info');
+let product7 = new Product('','Omega3', '49kr', 'images/tillskott/tillskott1.jpg', 'Lägg till', 'info');
+let product8 = new Product('','Omega3', '49kr', 'images/tillskott/tillskott2.jpg', 'Lägg till', 'info');
+let product9 = new Product('','Omega3', '49kr', 'images/tillskott/tillskott3.jpg', 'Lägg till', 'info');
+let product10 = new Product('','Omega3', '49kr', 'images/tillskott/tillskott4.jpg', 'Lägg till', 'info');
+let product11 = new Product('','Omega3', '49kr', 'images/tillskott/tillskott5.jpg', 'Lägg till', 'info');
+let product12 = new Product('','Omega3', '49kr', 'images/tillskott/tillskott6.jpg', 'Lägg till', 'info');
+let product13 = new Product('','Omega3', '49kr', 'images/tillskott/tillskott7.jpg', 'Lägg till', 'info');
 
 
-
-let products = [product1, product2, product3, product4, product5];
-=======
-let product1 = new Product('Omega3', '49kr', 'images/tillskott/coffein.jpg', 'Lägg till', 'info');
-let product2 = new Product('Omega3', '49kr', 'images/tillskott/tillskott1.jpg', 'Lägg till', 'info');
-let product3 = new Product('Omega3', '49kr', 'images/tillskott/tillskott2.jpg', 'Lägg till', 'info');
-let product4 = new Product('Omega3', '49kr', 'images/tillskott/tillskott3.jpg', 'Lägg till', 'info');
-let product5 = new Product('Omega3', '49kr', 'images/tillskott/tillskott4.jpg', 'Lägg till', 'info');
-let product6 = new Product('Omega3', '49kr', 'images/tillskott/tillskott5.jpg', 'Lägg till', 'info');
-let product7 = new Product('Omega3', '49kr', 'images/tillskott/tillskott6.jpg', 'Lägg till', 'info');
-let product8 = new Product('Omega3', '49kr', 'images/tillskott/tillskott7.jpg', 'Lägg till', 'info');
-
-
-
-let products = [product1, product2, product3, product4, product5, product6, product7, product8];
->>>>>>> Mikael
+let products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, product13];
 
 for (let i = 0; i < products.length; i++) {
 	let newElement = createElement(products[i]);
--- JavaScript/script-Utrustning.js
+++ JavaScript/script-Utrustning.js
@@ -8,28 +8,20 @@ function Product(brand, name, price, img, bye, info) {
 
 }
 
-<<<<<<< HEAD
-let product1 = new Product('GAIAM', 'Grey Hamsa Yoga Mat 4mm', '299 :-', 'images/utrustning/yogaMatta.jpg', 'Lägg till', 'info');
-let product2 = new Product('GARMIN', 'Vivoactive 4 S', '3249 :-', 'images/utrustning/pulsklocka.jpg', 'Lägg till', 'info');
-let product3 = new Product('ABILICA', 'Door Gym', '499 :-', 'images/utrustning/doorGym.jpg', 'Lägg till', 'info');
-let product4 = new Product('Casall', 'Tube Roll', '599 :-', 'images/utrustning/tubeRoll.jpg', 'Lägg till', 'info');
-let product5 = new Product('CASALL', 'Eco Cold Bottle', '199 :-', 'images/utrustning/bottle.jpg', 'Lägg till', 'info');
-let product6 = new Product('NIKE', 'Push Up Grip 3.0', '299 :-', 'images/utrustning/pushUpGrip.jpg', 'Lägg till', 'info');
-let product7 = new Product('IRON GYM', 'Fixed Hex Dumbbell 4kg Pair', '299 :-', 'images/utrustning/dumbbell.jpg', 'Lägg till', 'info');
+let product1 = new Product('', 'Hörlurar', '1149kr', 'images/utrustning/utrustning1.jpg', 'Lägg till', 'info');
+let product2 = new Product('', 'Hantel 3kg', '249kr', 'images/utrustning/utrustning2.jpg', 'Lägg till', 'info');
+let product3 = new Product('', 'Gummiband', '49kr', 'images/utrustning/utrustning3.jpg', 'Lägg till', 'info');
+let product4 = new Product('', 'Vikter', '449kr', 'images/utrustning/utrustning4.jpg', 'Lägg till', 'info');
+let product5 = new Product('GAIAM', 'Grey Hamsa Yoga Mat 4mm', '299 :-', 'images/utrustning/yogaMatta.jpg', 'Lägg till', 'info');
+let product6 = new Product('GARMIN', 'Vivoactive 4 S', '3249 :-', 'images/utrustning/pulsklocka.jpg', 'Lägg till', 'info');
+let product7 = new Product('ABILICA', 'Door Gym', '499 :-', 'images/utrustning/doorGym.jpg', 'Lägg till', 'info');
+let product8 = new Product('Casall', 'Tube Roll', '599 :-', 'images/utrustning/tubeRoll.jpg', 'Lägg till', 'info');
+let product9 = new Product('CASALL', 'Eco Cold Bottle', '199 :-', 'images/utrustning/bottle.jpg', 'Lägg till', 'info');
+let product10 = new Product('NIKE', 'Push Up Grip 3.0', '299 :-', 'images/utrustning/pushUpGrip.jpg', 'Lägg till', 'info');
+let product11 = new Product('IRON GYM', 'Fixed Hex Dumbbell 4kg Pair', '299 :-', 'images/utrustning/dumbbell.jpg', 'Lägg till', 'info');
 
 
-
-let products = [product1, product2, product3, product4, product5, product6, product7];
-=======
-let product1 = new Product('Hörlurar', '1149kr', 'images/utrustning/utrustning1.jpg', 'Lägg till', 'info');
-let product2 = new Product('Hantel 3kg', '249kr', 'images/utrustning/utrustning2.jpg', 'Lägg till', 'info');
-let product3 = new Product('Gummiband', '49kr', 'images/utrustning/utrustning3.jpg', 'Lägg till', 'info');
-let product4 = new Product('Vikter', '449kr', 'images/utrustning/utrustning4.jpg', 'Lägg till', 'info');
-
-
-
-let products = [product1, product2, product3, product4];
->>>>>>> Mikael
+let products = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11];
 
 for (let i = 0; i < products.length; i++) {
 	let newElement = createElement(products[i]);
