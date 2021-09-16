3d48918ae6b404f9b1745dc75ac8e39a7877b17a
MikaelNorrman <mikael.norrman@gmail.com>
2021-09-16 12:03:47 +0200

    Fler produkter till tillskott.

--- JavaScript/script-Tillskott.js
+++ JavaScript/script-Tillskott.js
@@ -8,12 +8,17 @@ function Product(name, price, img, bye, info) {
 }
 
 let product1 = new Product('Omega3', '49kr', 'images/tillskott/coffein.jpg', 'Lägg till', 'info');
-let product2 = new Product('Omega3', '49kr', 'images/tillskott/coffein.jpg', 'Lägg till', 'info');
-let product3 = new Product('Omega3', '49kr', 'images/tillskott/coffein.jpg', 'Lägg till', 'info');
+let product2 = new Product('Omega3', '49kr', 'images/tillskott/tillskott1.jpg', 'Lägg till', 'info');
+let product3 = new Product('Omega3', '49kr', 'images/tillskott/tillskott2.jpg', 'Lägg till', 'info');
+let product4 = new Product('Omega3', '49kr', 'images/tillskott/tillskott3.jpg', 'Lägg till', 'info');
+let product5 = new Product('Omega3', '49kr', 'images/tillskott/tillskott4.jpg', 'Lägg till', 'info');
+let product6 = new Product('Omega3', '49kr', 'images/tillskott/tillskott5.jpg', 'Lägg till', 'info');
+let product7 = new Product('Omega3', '49kr', 'images/tillskott/tillskott6.jpg', 'Lägg till', 'info');
+let product8 = new Product('Omega3', '49kr', 'images/tillskott/tillskott7.jpg', 'Lägg till', 'info');
 
 
 
-let products = [product1, product2, product3];
+let products = [product1, product2, product3, product4, product5, product6, product7, product8];
 
 for (let i = 0; i < products.length; i++) {
 	let newElement = createElement(products[i]);
