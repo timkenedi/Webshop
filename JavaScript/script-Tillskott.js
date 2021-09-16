5a2a3e08c4c53ce789263359b7ff111b9c5f8744
VerGui <veronica.guizzardi@gmail.com>
2021-09-16 11:21:28 +0200

    Lagt till kläder

---
+++ JavaScript/script-Tillskott.js
@@ -0,0 +1,38 @@
+function Product(name, price, img, bye, info) {
+	this.name = name,
+	this.price = price,
+	this.img = img,
+	this.bye = bye,
+	this.info = info
+
+}
+
+let product1 = new Product('', '', '', '', '');
+
+
+
+let products = [product1, product2, product3];
+
+for (let i = 0; i < products.length; i++) {
+	let newElement = createElement(products[i]);
+	appendElementToHTML(newElement);
+}
+
+function createElement(product) {
+	let newElement = document.createElement('div');
+	newElement.className = 'product';
+	newElement.innerHTML = `<img src="${product.img}">
+							<div class="productInfo">
+								<h3> ${product.name}</h3>
+								<h3> ${product.price}</h3>
+								<button>${product.bye}</button>
+								<button>${product.info}</button>
+							</div>`;
+
+	return newElement;
+}
+
+function appendElementToHTML(element) {
+	let content = document.getElementById('content');
+	content.appendChild(element);
+}
