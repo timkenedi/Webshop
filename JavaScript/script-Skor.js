0ec3ccfdd39b47b1614c36fc6501cb26e533209b
VerGui <veronica.guizzardi@gmail.com>
2021-09-16 12:56:08 +0200

    lagt till produkter i utrustning

--- JavaScript/script-Skor.js
+++ JavaScript/script-Skor.js
@@ -1,4 +1,5 @@
-function Product(name, price, img, bye, info) {
+function Product(brand, name, price, img, bye, info) {
+	this.brand = brand,
 	this.name = name,
 	this.price = price,
 	this.img = img,
@@ -7,9 +8,9 @@ function Product(name, price, img, bye, info) {
 
 }
 
-let product1 = new Product('Air Max 90', '1299kr', 'images/skor/airmax.JPG', 'Lägg till', 'info');
-let product2 = new Product('Air Force 1', '1150kr', 'images/skor/airforce.JPG', 'Lägg till', 'info');
-let product3 = new Product('Jordan 1985', '30000kr', 'images/skor/jordan.JPG', 'Lägg till', 'info')
+let product1 = new Product('NIKE', 'Air Max 90', '1299kr', 'images/skor/airmax.JPG', 'Lägg till', 'info');
+let product2 = new Product('NIKE','Air Force 1', '1150kr', 'images/skor/airforce.JPG', 'Lägg till', 'info');
+let product3 = new Product('NIKE','Jordan 1985', '30000kr', 'images/skor/jordan.JPG', 'Lägg till', 'info')
 
 
 let products = [product1, product2, product3];
@@ -24,6 +25,7 @@ function createElement(product) {
 	newElement.className = 'product';
 	newElement.innerHTML = `<img src="${product.img}">
 							<div class="productInfo">
+								<h3> ${product.brand}</h3>
 								<h3> ${product.name}</h3>
 								<h3> ${product.price}</h3>
 								<button>${product.bye}</button>
--- JavaScript/script-Utrustning.js
+++ JavaScript/script-Utrustning.js
@@ -1,4 +1,5 @@
-function Product(name, price, img, bye, info) {
+function Product(brand, name, price, img, bye, info) {
+	this.brand = brand,
 	this.name = name,
 	this.price = price,
 	this.img = img,
@@ -7,11 +8,17 @@ function Product(name, price, img, bye, info) {
 
 }
 
-let product1 = new Product('', '', '', '', '');
+let product1 = new Product('GAIAM', 'Grey Hamsa Yoga Mat 4mm', '299 :-', 'images/utrustning/yogaMatta.jpg', 'Lägg till', 'info');
+let product2 = new Product('GARMIN', 'Vivoactive 4 S', '3249 :-', 'images/utrustning/pulsklocka.jpg', 'Lägg till', 'info');
+let product3 = new Product('ABILICA', 'Door Gym', '499 :-', 'images/utrustning/doorGym.jpg', 'Lägg till', 'info');
+let product4 = new Product('Casall', 'Tube Roll', '599 :-', 'images/utrustning/tubeRoll.jpg', 'Lägg till', 'info');
+let product5 = new Product('CASALL', 'Eco Cold Bottle', '199 :-', 'images/utrustning/bottle.jpg', 'Lägg till', 'info');
+let product6 = new Product('NIKE', 'Push Up Grip 3.0', '299 :-', 'images/utrustning/pushUpGrip.jpg', 'Lägg till', 'info');
+let product7 = new Product('IRON GYM', 'Fixed Hex Dumbbell 4kg Pair', '299 :-', 'images/utrustning/dumbbell.jpg', 'Lägg till', 'info');
 
 
 
-let products = [product1, product2, product3];
+let products = [product1, product2, product3, product4, product5, product6, product7];
 
 for (let i = 0; i < products.length; i++) {
 	let newElement = createElement(products[i]);
@@ -23,6 +30,7 @@ function createElement(product) {
 	newElement.className = 'product';
 	newElement.innerHTML = `<img src="${product.img}">
 							<div class="productInfo">
+								<h3> ${product.brand}</h3>
 								<h3> ${product.name}</h3>
 								<h3> ${product.price}</h3>
 								<button>${product.bye}</button>
